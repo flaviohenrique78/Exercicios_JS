dfa609526bf2b44137086cd61b8d3439f04dc410
flaviohenrique78 <flaviohe7@gmail.com>
2021-09-30 21:58:09 -0300

    ex_aula

---
+++ ex_aula_01.js
@@ -0,0 +1,35 @@
+// ---------------------------------------- Exercicio_01_JS_básico-------------------------------------------------
+
+// a. Imprima no console a multiplicação de 42 por -3.14.
+// resposta
+
+console.log(42 * -3.14)
+
+//b. Atribua a uma constante a soma das strings "Oter" e "wise" e imprima no console seu valor 
+// resposta
+
+
+const f = "Hello" + " " + "Word"
+
+console.log()
+
+//c. Atribua a uma variavel o resultado da operação AND entre os valores booleanos true e false e imprima no console
+// resposta
+
+let x = true && false
+
+console.log(x)
+
+//d. Atribua a uma variável o resultado da operação OR entre os valores booleanos true e false e imprima seu valor no console.
+// resposta
+
+let or = true || false
+
+console.log(or)
+
+//e. Imprima no console o resultado da operação NOT com o valor true
+// resposta
+
+let not = !false
+
+console.log(not)
